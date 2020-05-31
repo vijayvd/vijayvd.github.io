432f4860e6c8d275f84408faf18bd4da0fc805b9
Vijay Desai <vijayvd@vijayvd-macbookpro.roam.corp.google.com>
2020-05-30 23:14:16 -0700

    a) update to path in convert_and_move.sh, b) remove Quantile*.ipynb because of file name issue.

--- _jupyter/convert_and_move.sh
+++ _jupyter/convert_and_move.sh
@@ -1,5 +1,5 @@
 nb=$1
 
 jupyter nbconvert $nb --to markdown
-mv ${nb%.ipynb}.md ~/github/cduvallet.github.io/_posts/
-mv ${nb%.ipynb}_files ~/github/cduvallet.github.io/images/
+mv ${nb%.ipynb}.md ~/vijayvd.github.io/_posts/
+mv ${nb%.ipynb}_files ~/vijayvd.github.io/images/
